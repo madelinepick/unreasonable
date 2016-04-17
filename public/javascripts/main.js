25eed981f869cce4ae17f1be4b5cde8b40abd420
Madeline Pickering <madelinepickering@gmail.com>
2016-04-17 14:44:23 -0600

    initial comit

---
+++ public/javascripts/main.js
@@ -0,0 +1,27 @@
+$(function(){
+  var value = $('.hero').css('height');
+  console.log(value);
+  $('.leftcol').css("min-height", value);
+  $(".rightcol").css("min-height", value);
+  
+  $(".story").on('click', function(){
+    $('html, body').animate({
+        scrollTop: $("#story").offset().top
+    }, 2000);
+  })
+  $(".why").on('click', function(){
+    $('html, body').animate({
+        scrollTop: $("#why").offset().top
+    }, 2000);
+  })
+  $(".skills").on('click', function(){
+    $('html, body').animate({
+        scrollTop: $("#skills").offset().top
+    }, 2000);
+  })
+  $(".elephant").on('click', function(){
+    $('html, body').animate({
+        scrollTop: $("#elephant").offset().top
+    }, 2000);
+  })
+});
