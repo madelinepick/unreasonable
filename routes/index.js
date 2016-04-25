a6fc6877efdafaf9e89e166a2a2d663d93f2f361
Madeline Pickering <madelinepickering@gmail.com>
2016-04-25 13:53:01 -0600

    trial project

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Unreasonable' });
+});
+
+router.get('/trial-project', function(req, res, next) {
+  res.render('trial', { title: 'Unreasonable' });
 });
 
 module.exports = router;
